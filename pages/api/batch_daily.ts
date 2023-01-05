695717d4faa77e878215df28fc68ae531ecde74c
SEEWON <andyaaa78@gmail.com>
2023-01-05 15:32:44 +0900

    [GIF-79] Fix: 한국 Timezone에 맞는 데이터 추가(Github Actions)

--- pages/api/batch_daily.ts
+++ pages/api/batch_daily.ts
@@ -1,14 +1,19 @@
 import { insertOneInCollection, updateOneInCollection } from './db';
 
 const dataInit = () => {
-  const date = new Date();
-  const m = date.getMonth() + 1;
-  const d = date.getDate();
-  const today = `${date.getFullYear()}${m > 9 ? '' : 0}${m}${
+  //Github Actions Runner가 수행되는 환경은 UTC+0 환경이므로,
+  //한국 시간 기준으로 Data를 추가하기 위한 Datetime 설정
+  const curr_d = new Date();
+  const utc = curr_d.getTime() + curr_d.getTimezoneOffset() * 60 * 1000;
+  const kr_time_dff = 9 * 60 * 60 * 1000; // UTC+9 시간대 만큼의 Time offset
+  const kr_d = new Date(utc + kr_time_dff);
+  const m = kr_d.getMonth() + 1;
+  const d = kr_d.getDate();
+  const today = `${kr_d.getFullYear()}${m > 9 ? '' : 0}${m}${
     d > 9 ? '' : 0
   }${d}`;
 
-  const isWeekend = date.getDay() === 0 || date.getDay() === 6 ? true : false;
+  const isWeekend = kr_d.getDay() === 0 || kr_d.getDay() === 6 ? true : false;
 
   const data = {
     date: today,
@@ -24,11 +29,17 @@ const dataInit = () => {
 };
 
 const modify_yesterday = async () => {
-  const date = new Date();
-  date.setDate(date.getDate() - 1);
-  const m = date.getMonth() + 1;
-  const d = date.getDate();
-  const yesterday = `${date.getFullYear()}${m > 9 ? '' : 0}${m}${
+  //Github Actions Runner가 수행되는 환경은 UTC+0 환경이므로,
+  //한국 시간 기준으로 Data를 추가하기 위한 Datetime 설정
+  const yesterday_d = new Date();
+  yesterday_d.setDate(yesterday_d.getDate() - 1);
+  const utc =
+    yesterday_d.getTime() + yesterday_d.getTimezoneOffset() * 60 * 1000;
+  const kr_time_dff = 9 * 60 * 60 * 1000; // UTC+9 시간대 만큼의 Time offset
+  const kr_d = new Date(utc + kr_time_dff);
+  const m = kr_d.getMonth() + 1;
+  const d = kr_d.getDate();
+  const yesterday = `${kr_d.getFullYear()}${m > 9 ? '' : 0}${m}${
     d > 9 ? '' : 0
   }${d}`;
 
