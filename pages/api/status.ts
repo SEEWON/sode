489441bb7475f47142f0626bd624f008062b05c1
SEEWON <andyaaa78@gmail.com>
2023-01-20 11:22:58 +0900

    [GIF-239] Fix: 오전 9시 이전 체크 불가한 Bug fix, UTC+9 Timezone offset 적용

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -3,10 +3,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { updateOneInCollection } from './db';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
-  const date = new Date();
-  const m = date.getMonth() + 1;
-  const d = date.getDate();
-  const today = `${date.getFullYear()}${m > 9 ? '' : 0}${m}${
+  const curr_d = new Date();
+  const utc = curr_d.getTime() + curr_d.getTimezoneOffset() * 60 * 1000;
+  const kr_time_dff = 9 * 60 * 60 * 1000; // UTC+9 시간대 만큼의 Time offset
+  const kr_d = new Date(utc + kr_time_dff);
+  const m = kr_d.getMonth() + 1;
+  const d = kr_d.getDate();
+  const today = `${kr_d.getFullYear()}${m > 9 ? '' : 0}${m}${
     d > 9 ? '' : 0
   }${d}`;
   const query = { date: today };
